98f7b5412537f66beb936c3089fa126642ddee79
MaxWell <xaxaxa25420@gmail.com>
2022-03-14 09:49:35 +0300

    process

--- src/main/java/app/controller/RestController.java
+++ src/main/java/app/controller/RestController.java
@@ -4,11 +4,13 @@ import app.model.Role;
 import app.model.User;
 import app.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 @org.springframework.web.bind.annotation.RestController
 //@RequestMapping(value = "/admin")
@@ -18,49 +20,49 @@ public class RestController {
     UserService userService;
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
-    @ResponseBody
+//    @ResponseBody
     public List<User> list() {
         return userService.getUsersList();
     }
 
-//    @GetMapping("{id}")
-//    public User getUserInfo(@PathVariable Long id) {
-//        return userService.getUser(id);
-//    }
+    @GetMapping("/get/currentUser")
+    public User getUserInfo() {
+        return (User) userService.loadUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+    }
 
-    @RequestMapping(value = "create/admin", method = RequestMethod.POST)
-    public void createAdmin(@RequestBody User user) {
+    @PostMapping("{role}")
+    public void create(@RequestBody User user, @PathVariable String role) {
         Collection<Role> roles = new ArrayList<>();
-        Role roleAdmin = new Role();
-        roleAdmin.setRole("ROLE_ADMIN");
-        roles.add(roleAdmin);
+        if (Objects.equals(role, "ADMIN_USER")) {
+            Role roleAdmin = new Role();
+            roleAdmin.setRole("ROLE_ADMIN");
+            roles.add(roleAdmin);
+        }
         Role roleUser = new Role();
         roleUser.setRole("ROLE_USER");
         roles.add(roleUser);
         user.setRoles(roles);
-        System.out.println(user.getAuthorities());
         userService.add(user);
     }
 
-    @RequestMapping(value = "create/user", method = RequestMethod.POST)
-    public void createUser(@RequestBody User user) {
+    @PutMapping("{role}")
+    public User update(@RequestBody User user, @PathVariable String role) {
         Collection<Role> roles = new ArrayList<>();
+        if (Objects.equals(role, "ADMIN_USER")) {
+            Role roleAdmin = new Role();
+            roleAdmin.setRole("ROLE_ADMIN");
+            roles.add(roleAdmin);
+        }
         Role roleUser = new Role();
         roleUser.setRole("ROLE_USER");
         roles.add(roleUser);
         user.setRoles(roles);
-        System.out.println(user.getAuthorities());
-        userService.add(user);
-    }
-
-    @PutMapping
-    public User update(@RequestBody User user) {
         userService.changeUser(user);
         return user;
     }
 
     @DeleteMapping("{id}")
-    public void delete(@RequestBody String id){
+    public void delete(@PathVariable String id){
         userService.removeUser(Long.valueOf(id));
     }
 }
